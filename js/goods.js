0a1717af2889b9a892835a4172862196bb271832
gaubee <gaubeebangeel@gmail.com>
2013-07-11 12:07:18 +0800

    在服务器上清空less，提交page-footer，在main.js中保存通用网站基本信息
    Signed-off-by: gaubee <gaubeebangeel@gmail.com>

--- js/goods.js
+++
@@ -1,6 +0,0 @@
-avalon.define("app", function(vm) {
-    vm.a = '123';
-    vm.click = function() {
-        vm.a = new Date - 0
-    }
-})
--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-
+avalon.define("soojoy", function(vm) {
+    vm.shop_name = "Soojoy E-commerce Website";
+    vm.author = "Gaubee";
+    vm.partners = [
+    	{name:"买否网",url:"http://www.maifou.net/"},
+    	{name:"免费开独立网店",url:"http://www.wdwd.com/"}
+    ];
+    vm.copyright = "© 2005-2013 ECSHOP 版权所有，并保留所有权利。"
+    vm.shop_address = "http://gaubee.github.io/soojoy";
+    vm.shop_postcode = "100000";
+    vm.service_phone = "18010001000";
+    vm.service_email = "gaubeebangeel@gmail.com"
+    vm.im = "654393589";
+    vm.icp_number = "CT-8452110";
+})
